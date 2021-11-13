cee3742d2229e99f939ea79283501fc6f9fc0f9e
Donald Johnson <donaldjohnson.nz@gmail.com>
2021-11-14 11:38:00 +1300

    update: call zsh

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -3,11 +3,7 @@
 set -e
 PATH=$PATH:~/.local/bin
 
-echo "Configure pre-commit"
-pre-commit install --install-hooks
-
-echo "==> Install additional pip packages"
+[ -s ./.devcontainer/pip-requirements.txt ] && echo "Configure pre-commit" pre-commit install --install-hooks || echo "==> No pre-commit to install"
 [ -s ./.devcontainer/pip-requirements.txt ] && echo "==> Install pip packages" && pip3 install --no-cache-dir -r ./.devcontainer/pip-requirements.txt || echo "==> No additional packages to install"
-
-echo "set permissions on gnupg"
-sudo chown generic:generic /home/generic/.gnupg
+sudo chown generic:generic /home/generic/.gnupg && echo "==> Set permissions on gnupg" || echo "==> No permissions to set on gnupg"
+zsh
