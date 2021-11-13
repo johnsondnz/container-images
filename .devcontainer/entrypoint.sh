64a503a90d927c345311f4698334540f1a7502e8
Donald Johnson <donaldjohnson.nz@gmail.com>
2021-11-14 11:04:57 +1300

    configure devcontainer for vscode

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -6,7 +6,8 @@ PATH=$PATH:~/.local/bin
 echo "Configure pre-commit"
 pre-commit install --install-hooks
 
+echo "==> Install additional pip packages"
+[ -s ./.devcontainer/pip-requirements.txt ] && echo "==> Install pip packages" && pip3 install --no-cache-dir -r ./.devcontainer/pip-requirements.txt || echo "==> No additional packages to install"
+
 echo "set permissions on gnupg"
 sudo chown generic:generic /home/generic/.gnupg
-
-echo "==> Running container version $VERSION"
