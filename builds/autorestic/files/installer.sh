4351b0df545bbdc9f2d4488c98f47914bb73c4a9
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-03-30 02:32:19 +0000

    add apt clean

--- builds/autorestic/files/installer.sh
+++ builds/autorestic/files/installer.sh
@@ -25,6 +25,7 @@ echo "==> Cleanup"
 apt purge -yqq git
 apt autoremove -yqq --purge
 apt autoclean
+apt clean
 rm -rf /var/lib/apt/lists/*
 rm /etc/pip-requirements.txt
 rm /etc/apt-requirements.txt
