bb0a24d0dc4fd4114ac4b25422d9f9ace833ff2b
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-03-30 16:30:46 +1300

    update entrypoint

--- builds/sonarr_cleaner/files/entrypoint.sh
+++ builds/sonarr_cleaner/files/entrypoint.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-echo "==> Setup Crontab with $CRON"
-echo 'PATH="/usr/local/bin:/usr/bin:/bin"' > /crontab
-echo "$CRONTAB >/proc/1/fd/1 2>/proc/1/fd/2" >> /crontab
-
-echo "==> Setup timezone"
-cp /usr/share/zoneinfo/$TZ /etc/localtime || echo 'Nothing to do'
-
 if [ $# -eq 0 ]; then
-  if [ -f /config/config.yaml ]; then
-    echo "==> Initiliase restic"
-    autorestic check -c /config/config.yaml --verbose
-
-    echo "==> Parsing and loading cron"
-    /usr/bin/crontab /crontab
-
-    echo "==> Get cron settings"
-    /usr/bin/crontab -l
-
-    echo "==> Starting cron in foreground"
-    /usr/sbin/cron -f -l 2
-  else
-    echo "==> Restic configuration not found in '/config/config/yaml'"
-    echo "Exiting..."
-    exit 1
-  fi
+  python3 /app/main.py
 
 elif [ $1 == 'bash' ]; then
   zsh
