04dacd1b17a6124715f0bc14b7e72598f36147d6
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-03-30 16:59:54 +1300

    add embedded cron

--- builds/sonarr_cleaner/files/entrypoint.sh
+++ builds/sonarr_cleaner/files/entrypoint.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+echo "==> Setup Crontab with $CRON"
+echo 'PATH="/usr/local/bin:/usr/bin:/bin"' > /crontab
+echo "$CRONTAB >/proc/1/fd/1 2>/proc/1/fd/2" >> /crontab
+
+echo "==> Setup timezone"
+cp /usr/share/zoneinfo/$TZ /etc/localtime || echo 'Nothing to do'
+
 if [ $# -eq 0 ]; then
-  python3 /app/main.py
+  if [ -f /app/config/main.yml ]; then
+    echo "==> Parsing and loading cron"
+    /usr/bin/crontab /crontab
+
+    echo "==> Get cron settings"
+    /usr/bin/crontab -l
+
+    echo "==> Starting cron in foreground"
+    /usr/sbin/cron -f -l 2
+  else
+    echo "==> Restic configuration not found in '/config/config/yaml'"
+    echo "Exiting..."
+    exit 1
+  fi
 
 elif [ $1 == 'bash' ]; then
   zsh
--- builds/sonarr_cleaner/packer.pkr.hcl
+++ builds/sonarr_cleaner/packer.pkr.hcl
@@ -31,6 +31,8 @@ source "docker" "container_image" {
   commit = true
   pull   = true
   changes = [
+    "ENV CRONTAB * * * * * echo 'hello-world'",
+    "ENV TZ UTC",
     "WORKDIR /home/${var.default_user}",
     "ENTRYPOINT [\"/entrypoint.sh\"]",
   ]
