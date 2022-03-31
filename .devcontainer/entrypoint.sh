0ca370f0361232e82e458a7633bb62decefab0d7
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-03-31 02:23:14 +0000

    fixed

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -5,4 +5,5 @@ PATH=$PATH:~/.local/bin
 
 [ -s .pre-commit-config.yaml ] && echo "Configure pre-commit" pre-commit install --install-hooks || echo "==> No pre-commit to install"
 [ -s ./.devcontainer/pip-requirements.txt ] && echo "==> Install pip packages" && pip3 install --no-cache-dir -r ./.devcontainer/pip-requirements.txt || echo "==> No additional packages to install"
+sudo chown generic:generic /home/generic/.gnupg && echo "==> Set permissions on gnupg" || echo "==> No permissions to set on gnupg"
 zsh
