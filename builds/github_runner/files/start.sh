e231a65b40561d090ed5242c5fe2d9e533c5d4b5
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-04-09 06:31:29 +0000

    support for orgs and individuals

--- builds/github_runner/files/start.sh
+++ builds/github_runner/files/start.sh
@@ -2,15 +2,36 @@
 
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
+GITHUB_USERNAME=$GITHUB_USERNAME
+REPOSITORY=$REPOSITORY
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+if [[ -z $ORGANIZATION ]] && [[ -z $GITHUB_USERNAME ]]; then
+    echo "Set either ORGANIZATION or GITHUB_USERNAME with REPOSITORY, not both... exiting"
+    exit 1
+fi
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+org() {
+    echo "Entering organization mode..."
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+}
+
+individual() {
+    echo "Entering individual with repository mode..."
+    REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_USERNAME}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+    ./config.sh --url https://github.com/${GITHUB_USERNAME}/${REPOSITORY} --token ${REG_TOKEN}
+}
 
 cleanup() {
     echo "Removing runner..."
     ./config.sh remove --unattended --token ${REG_TOKEN}
 }
+
+if [[ -z $ORGANIZATION ]]; then
+    org
+elif [[ -z $GITHUB_USERNAME ]]; then
+    individual
+fi
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
