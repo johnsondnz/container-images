51171a959d1c3547102c8ee6e6965467602e283d
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-04-09 05:39:24 +0000

    set new entrypoint so build tests work

--- builds/github_runner/files/entrypoint.sh
+++ builds/github_runner/files/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
-
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
-
-cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
-}
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-./run.sh & wait $!
+if [ $# -eq 0 ]; then
+  bash /start.sh
+elif [ $1 == 'bash' ]; then
+  zsh
+elif [ $1 == 'shell' ]; then
+  zsh
+elif [ $1 == 'sh' ]; then
+  zsh
+elif [ $1 == 'zsh' ]; then
+  zsh
+fi
