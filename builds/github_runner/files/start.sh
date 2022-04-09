047619595106b3ffc18123cf9df745717b375290
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-04-09 08:32:13 +0000

    fixes pathing

--- builds/github_runner/files/start.sh
+++ builds/github_runner/files/start.sh
@@ -13,18 +13,18 @@ fi
 org() {
     echo "Entering organization mode..."
     REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-    ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+    ./opt/actions-runner/config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 }
 
 individual() {
     echo "Entering individual with repository mode..."
     REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${GITHUB_USERNAME}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
-    ./config.sh --url https://github.com/${GITHUB_USERNAME}/${REPOSITORY} --token ${REG_TOKEN}
+    ./opt/actions-runner/config.sh --url https://github.com/${GITHUB_USERNAME}/${REPOSITORY} --token ${REG_TOKEN}
 }
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./opt/actions-runner/config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 if ! [[ -z $ORGANIZATION ]]; then
