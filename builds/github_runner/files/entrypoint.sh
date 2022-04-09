8dc659574ae5ffab698d05b635e29fcfe4bcc8f3
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-04-09 05:16:33 +0000

    set install location

--- builds/github_runner/files/entrypoint.sh
+++ builds/github_runner/files/entrypoint.sh
@@ -5,7 +5,7 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/generic/actions-runner
+cd /opt/actions-runner
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
