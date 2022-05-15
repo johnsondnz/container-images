17a3d9c682d8bcb779238829edc2910513a537ba
Donald Johnson <donaldjohnson.nz@gmail.com>
2022-05-15 20:01:22 +1200

    Update installer.sh

--- builds/github_runner/files/installer.sh
+++ builds/github_runner/files/installer.sh
@@ -34,7 +34,7 @@ usermod -aG docker generic
 echo "==> Install hashicorp repo"
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get install -y terraform
+apt-get update && apt-get install -y terraform
 
 echo "==> Cleanup"
 apt-get autoremove -yqq --purge
